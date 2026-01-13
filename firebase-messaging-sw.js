2aeceacde60bbdb033c683755f1b94ddf5a0c185
resoner0796 <ulisees.luna96@gmail.com>
2026-01-13 04:44:23 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Configuración (LA MISMA DE TU INDEX)
 firebase.initializeApp({
     apiKey: "AIzaSyDahnSPvBNTYot00JCn5CBjggAYFVGhbjE",
     authDomain: "panel-logistica-simple.firebaseapp.com",
@@ -13,14 +12,12 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// Esto maneja la notificación cuando la app está en segundo plano
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Notificación recibida:', payload);
+  console.log('[firebase-messaging-sw.js] Notificación:', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/1041/1041883.png' // Icono genérico de caja
+    icon: 'https://cdn-icons-png.flaticon.com/512/1041/1041883.png'
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
